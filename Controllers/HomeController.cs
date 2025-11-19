5c375c3f17533822e070e225758557345cb028f0
Florian <florian.popescu.2004@gmail.com>
2025-11-19 20:23:53 +0200

    afisarea pe harta a stanelor si incarcarea produselor din zona

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StanaGO.Data;
 using StanaGO.Models;
 using System.Diagnostics;
 
@@ -8,16 +10,70 @@ namespace StanaGO.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly StanaGOContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, StanaGOContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [Authorize]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetNearbyData(double lat, double lng)
+        {
+            var allFarms = await _context.Sheepfarms
+                                         .Include(f => f.Products)
+                                         .ToListAsync();
+
+            var nearbyProducts = new List<object>();
+
+            foreach (var farm in allFarms)
+            {
+                double distance = CalculateDistance(lat, lng, farm.Latitude, farm.Longitude);
+
+                if (distance <= 30000)
+                {
+                    foreach (var prod in farm.Products)
+                    {
+                        nearbyProducts.Add(new
+                        {
+                            Name = prod.Name,
+                            Price = prod.Price,
+                            Image = prod.ImagePath,
+                            FarmName = farm.Name,
+                            Distance = (distance / 1000).ToString("0.0") + " km" 
+                        });
+                    }
+                }
+            }
+
+            return Json(new
+            {
+                farms = allFarms.Select(f => new { f.Name, f.Latitude, f.Longitude, f.Address }),
+                products = nearbyProducts
+            });
+        }
+
+        private double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
+        {
+            var R = 6371e3;
+            var radLat1 = lat1 * Math.PI / 180;
+            var radLat2 = lat2 * Math.PI / 180;
+            var deltaLat = (lat2 - lat1) * Math.PI / 180;
+            var deltaLon = (lon2 - lon1) * Math.PI / 180;
+
+            var a = Math.Sin(deltaLat / 2) * Math.Sin(deltaLat / 2) +
+                    Math.Cos(radLat1) * Math.Cos(radLat2) *
+                    Math.Sin(deltaLon / 2) * Math.Sin(deltaLon / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return R * c; 
         }
 
         [Authorize]
